1ac68e5aa32077391b82c93b035001a20ab9f40e
Silas Stoffel <silasstofel@gmail.com>
2020-12-15 21:05:52 -0300

    Quebrando a exclusão em métodos

--- app/Services/RemovedorDeSerie.php
+++ app/Services/RemovedorDeSerie.php
@@ -14,23 +14,30 @@ class RemovedorDeSerie
     {
         $serieExcluida = null;
         DB::transaction(function () use ($serieId, &$serieExcluida) {
-            $serie    = Serie::find($serieId);
-            // Remove temporadas
-            $serie->temporadas->each(function (Temporada $temporada) {
-                // 1. Remove Epsodios
-                $temporada->episodios->each(function (Episodio $epsodio) {
-                    $epsodio->delete();
-                });
-
-                // 2. Remove a temporada depois de ter removido os filhos (epsodios)
-                $temporada->delete();
-            });
-
-            // 3. Por fim, remove a série que é entidade principal
+            $serie = Serie::find($serieId);
+            $this->removerTemporadas($serie);
+            // Por fim, remove a série que é entidade principal
             $serieExcluida = $serie;
             $serie->delete();
         });
         return $serieExcluida;
+    }
+
+    private function removerTemporadas(Serie $serie)
+    {
+        $serie->temporadas->each(function (Temporada $temporada) {
+            // 1. Remove Epsodios
+            $this->removerEpisodios($temporada);
+            // 2. Remove a temporada depois de ter removido os filhos (epsodios)
+            $temporada->delete();
+        });
+    }
+
+    private function removerEpisodios(Temporada $temporada)
+    {
+        $temporada->episodios->each(function (Episodio $epsodio) {
+            $epsodio->delete();
+        });
     }
 
 }
