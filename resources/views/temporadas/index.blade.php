77eedd1f25ab089f0e1591a7c514325aad21b8fe
Silas Stoffel <silasstofel@gmail.com>
2020-12-20 13:32:11 -0300

    Contando a quantidade de epsódios assistidos

--- resources/views/temporadas/index.blade.php
+++ resources/views/temporadas/index.blade.php
@@ -13,7 +13,7 @@ Temporadas de {{$serie->nome}}
                 <li class="list-group-item">
                     <div class="d-flex justify-content-between align-items-center">
                         <a href="/temporadas/{{ $temporada->id }}/episodios">Temporada {{ $temporada->numero }}</a>
-                        <span class="badge badge-secondary">0/{{ $temporada->episodios->count()}}</span>
+                        <span class="badge badge-secondary">{{ $temporada->getEpisodiosAssistidos()->count() }} / {{ $temporada->episodios->count()}}</span>
                     </div>
                 </li>
             @endforeach
