2b917f14233c70310ab1f22585bd471fdb9bdea9
Silas Stoffel <silasstofel@gmail.com>
2020-12-21 21:02:26 -0300

    Habilitando auth - php artisan ui:auth

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,7 @@ Route::group(['prefix' => 'temporadas'], function() {
     Route::post('/{temporada}/episodios/assistir', [EpisodiosController::class, 'watch']);
 });
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
