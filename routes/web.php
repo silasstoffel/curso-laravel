7cf271cdba7eba8bc3551e5702f7eecb0d8bafcd
Silas Stoffel <silasstofel@gmail.com>
2020-12-22 21:03:20 -0300

    Protegendo as rotas com middlewares

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'series'], function() {
-    Route::get('/', [SeriesController::class, 'index'])->name('serie.index');
+Route::group(['prefix' => 'series', 'middleware' => 'auth'], function() {
+    Route::get('/', [SeriesController::class, 'index'])->name('serie.index')->middleware('auth');
     Route::get('/create', [SeriesController::class, 'create'])->name('serie_create');
     Route::post('/create', [SeriesController::class, 'store'])->name('serie_store');
     Route::delete('/remove/{id}', [SeriesController::class, 'destroy'])->name('serie_destroy');
@@ -18,7 +18,7 @@ Route::group(['prefix' => 'series'], function() {
     Route::get('/{serieId}/temporadas', [TemporadasContoller::class, 'index'])->name('temporada.index');
 });
 
-Route::group(['prefix' => 'temporadas'], function() {
+Route::group(['prefix' => 'temporadas', 'middleware' => 'auth'], function() {
     Route::get('/{temporada}/episodios', [EpisodiosController::class, 'index']);
     Route::post('/{temporada}/episodios/assistir', [EpisodiosController::class, 'watch']);
 });
