f2531d649cc4db41b3e81056ddc30ddab74cd46b
Silas Stoffel <silasstofel@gmail.com>
2021-01-17 11:40:02 -0300

    Excluindo capa da série com jos assíncrono

--- app/Jobs/ExcluirCapaSerieJob.php
+++ app/Jobs/ExcluirCapaSerieJob.php
@@ -10,7 +10,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Storage;
 
-class ExcluirCapaSerieJob /*implements ShouldQueue*/
+class ExcluirCapaSerieJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
